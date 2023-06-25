143703b6b222ec8afca640239b08304d3bd58311
Koen van Leeuwen <koen@logiqs.nl>
2023-06-25 12:45:09 +0200

    Part load had usually higher efficiency, that is relevant.

--- ConsoleOptimizer/Program.cs
+++ ConsoleOptimizer/Program.cs
@@ -10,27 +10,46 @@ public static class Program
     /// <param name="maxEnergy">Maximum battery capacity, in kWh</param>
     /// <param name="maxChargePower">Maximum charger power (DC side)</param>
     /// <param name="maxDischargePower">Maximum discharge power (DC side)</param>
-    public static void Main(double startEnergy = 2, double endEnergy = 1, double maxEnergy = 5, double maxChargePower = 2, double maxDischargePower = 2.2)
+    /// <param name="roundtripEfficiencyFullpower">Full power efficiency, the roundtrip efficiency is evenly distributed between charge- and discharge efficiencies</param>
+    /// <param name="roundtripEfficiencyHalfpower">Part load efficiency, usually higher than full load efficiency.</param>
+    public static void Main(double startEnergy = 0,
+                            double endEnergy = 0,
+                            double maxEnergy = 5,
+                            double maxChargePower = 2.2,
+                            double maxDischargePower = 1.7,
+                            double roundtripEfficiencyFullpower = 0.9,
+                            double roundtripEfficiencyHalfpower = 0.9
+        )
     {
-        HourPrice[] prices = HourPrice.FromAcPrices(SampleDays.Oct31_2022,
-         chargeEfficiency: 0.9, dischargeEfficiency: 0.9).ToArray();
+        HourPrice[] fullPowerPrices = HourPrice.FromAcPrices(SampleDays.Jun07_2023,
+         chargeEfficiency: Math.Sqrt(roundtripEfficiencyFullpower), dischargeEfficiency: Math.Sqrt(roundtripEfficiencyFullpower)).ToArray();
+        HourPrice[] halfPowerPrices = HourPrice.FromAcPrices(SampleDays.Jun07_2023,
+             chargeEfficiency: Math.Sqrt(roundtripEfficiencyHalfpower), dischargeEfficiency: Math.Sqrt(roundtripEfficiencyHalfpower)).ToArray();
 
         Solver solver = Solver.CreateSolver("GLOP");
 
-        var chargeEnergies = solver.MakeNumVarArray(prices.Length, 0, maxChargePower);
-        var dischargeEnergies = solver.MakeNumVarArray(prices.Length, 0, maxDischargePower);
+        var chargeFullpower = solver.MakeNumVarArray(fullPowerPrices.Length, 0, maxChargePower);
+        var dischargeFullpower = solver.MakeNumVarArray(fullPowerPrices.Length, 0, maxDischargePower);
+        var chargeHalfpower = solver.MakeNumVarArray(fullPowerPrices.Length, 0, 0.5 * maxChargePower);
+        var dischargeHalfpower = solver.MakeNumVarArray(fullPowerPrices.Length, 0, 0.5 * maxDischargePower);
 
         LinearExpr SoC = new LinearExpr() + startEnergy;
         LinearExpr profit = new();
-        for (int hour = 0; hour < prices.Length; hour++)
+        for (int hour = 0; hour < fullPowerPrices.Length; hour++)
         {
+            // Make sure total power is limited
+            solver.Add(chargeFullpower[hour] + chargeHalfpower[hour] <= maxChargePower);
+            solver.Add(dischargeFullpower[hour] + dischargeHalfpower[hour] <= maxDischargePower);
+
             // Add charge until full constraints
-            solver.Add(SoC + chargeEnergies[hour] <= maxEnergy);
-            solver.Add(SoC - dischargeEnergies[hour] >= 0);
-            SoC = SoC + chargeEnergies[hour] - dischargeEnergies[hour];
+            solver.Add(SoC + chargeFullpower[hour] + chargeHalfpower[hour] <= maxEnergy);
+            solver.Add(SoC - dischargeFullpower[hour] - dischargeHalfpower[hour] >= 0);
+            SoC = SoC + chargeFullpower[hour] - dischargeFullpower[hour];
+            SoC = SoC + chargeHalfpower[hour] - dischargeHalfpower[hour];
 
             // Calculate running profits
-            profit = profit - prices[hour].Charge * chargeEnergies[hour] + prices[hour].Discharge * dischargeEnergies[hour];
+            profit = profit - fullPowerPrices[hour].Charge * chargeFullpower[hour] + fullPowerPrices[hour].Discharge * dischargeFullpower[hour];
+            profit = profit - halfPowerPrices[hour].Charge * chargeHalfpower[hour] + halfPowerPrices[hour].Discharge * dischargeHalfpower[hour];
         }
         solver.Add(SoC >= endEnergy);
 
@@ -39,11 +58,11 @@ public static class Program
         var result = solver.Solve();
         Console.WriteLine(result.ToString());
 
-        Console.WriteLine($" Result: €{Math.Round(solver.Objective().Value(), 2)}");
-        PrintGraphs(startEnergy, maxEnergy, prices, chargeEnergies, dischargeEnergies);
+        Console.WriteLine($" Result: €{Math.Round(solver.Objective().Value(), 4)}");
+        PrintGraphs(startEnergy, maxEnergy, fullPowerPrices, chargeFullpower, dischargeFullpower, chargeHalfpower, dischargeHalfpower);
     }
 
-    private static void PrintGraphs(double lastSoC, double maxEnergy, HourPrice[] prices, Variable[] chargeEnergies, Variable[] dischargeEnergies)
+    private static void PrintGraphs(double lastSoC, double maxEnergy, HourPrice[] prices, Variable[] chargeEnergies, Variable[] dischargeEnergies, Variable[] chargeHalfpower, Variable[] dischargeHalfpower)
     {
         var minPrice = prices.Min(p => p.Charge);
         var maxPrice = prices.Max(p => p.Charge);
@@ -52,9 +71,10 @@ public static class Program
         {
             Console.Write($"{hour:00}   |");
             WriteGraphLine(prices[hour].Charge, minPrice, maxPrice);
-            WriteGraphLine(chargeEnergies[hour].SolutionValue(), 0, maxEnergy);
-            WriteGraphLine(dischargeEnergies[hour].SolutionValue(), 0, maxEnergy);
+            WriteGraphLine(chargeEnergies[hour].SolutionValue() + chargeHalfpower[hour].SolutionValue(), 0, maxEnergy);
+            WriteGraphLine(dischargeEnergies[hour].SolutionValue() + dischargeHalfpower[hour].SolutionValue(), 0, maxEnergy);
             lastSoC += chargeEnergies[hour].SolutionValue() - dischargeEnergies[hour].SolutionValue();
+            lastSoC += chargeHalfpower[hour].SolutionValue() - dischargeHalfpower[hour].SolutionValue();
             WriteGraphLine(lastSoC, 0, maxEnergy);
             Console.WriteLine();
         }
@@ -64,10 +84,10 @@ public static class Program
     {
         int width = 15;
         double part = width * ((value - min) / (max - min));
-        string line = new string('*', (int)Math.Floor(part));
+        string line = new string('*', (int)Math.Round(part));
         if (part - line.Length > 0.5)
             line += '-';
-        Console.Write($"{value+0.0001:0.00} ");
+        Console.Write($"{value + 0.0001:0.00} ");
         Console.Write(line);
         Console.Write(new string(' ', width - line.Length));
         Console.Write(" | ");
